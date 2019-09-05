42581f90bf48eee0fb7c7246fe0d8e8b2d95cc86
lucas-maijers <lucas.maijers@student.rocvf.nl>
2019-09-05 11:11:11 +0200

    H05 StaafDiagramVariabelen, Lucas Maijers

--- src/h05/StaafDiagramVariabelen.java
+++ src/h05/StaafDiagramVariabelen.java
@@ -9,55 +9,28 @@ public class StaafDiagramVariabelen extends Applet {
     Color Hans;
     Color Jeroen;
     int breedte;
-    // Gewicht Kleurstaven
-    int KG20;
-    int KG40;
-    int KG60;
-    int KG80;
-    int KG100;
-    int KG120;
-    int KG140;
-    int KG160;
-    // Gewicht Hoogte
-    int HKG20;
-    int HKG40;
-    int HKG60;
-    int HKG80;
-    int HKG100;
-    int HKG120;
-    int HKG140;
-    int HKG160;
-
+    int HoogteX;
+    int GewichtValerie;
+    int GewichtHans;
+    int GewichtJeroen;
 
     public void init() {
         // Kleuren
         Valerie = Color.MAGENTA;
         Hans = Color.blue;
         Jeroen = Color.GREEN;
-        // Breedte Kleurstaaf
+        // Breedte/Hoogte Staven
         breedte = 20;
-        // Gewicht Kleurstaven
-        KG160 = 399;
-        KG140 = 349;
-        KG120 = 299;
-        KG100 = 249;
-        KG80 = 199;
-        KG60 = 149;
-        KG40 = 99;
-        KG20 = 49;
-        // Gewicht Hoogte
-        HKG20 = 401;
-        HKG40 = 351;
-        HKG60 = 301;
-        HKG80 = 251;
-        HKG100 = 201;
-        HKG120 = 151;
-        HKG140 = 101;
-        HKG160 = 51;
+        HoogteX = 400;
+        // Gewicht Personen
+        GewichtHans = 100;
+        GewichtValerie = 50;
+        GewichtJeroen = 80;
     }
     public void paint(Graphics g) {
         // Diagram
-        g.drawRect(50, 50, 300, 400);
+        g.drawLine(50,50,50,400);
+        g.drawLine(50,HoogteX,HoogteX,HoogteX);
         // Verdeling
         g.drawString("20 KG", 0, 400);
         g.drawString("40 KG", 0, 350);
@@ -69,15 +42,15 @@ public class StaafDiagramVariabelen extends Applet {
         g.drawString("160 KG", 0, 50);
         // Valerie
         g.setColor(Valerie);
-        g.fillRect(76, HKG20, breedte, KG20);
+        g.fillRect(76, HoogteX - GewichtValerie, breedte, GewichtValerie);
         g.drawString("Valerie", 65, 465);
         // Jeroen
         g.setColor(Jeroen);
-        g.fillRect(151, HKG120, breedte, KG120);
+        g.fillRect(151, HoogteX - GewichtJeroen, breedte, GewichtJeroen);
         g.drawString("Jeroen", 140, 465);
         // Hans
         g.setColor(Hans);
-        g.fillRect(226, HKG80, breedte, KG80);
+        g.fillRect(226, HoogteX - GewichtHans, breedte, GewichtHans);
         g.drawString("Hans", 220, 465);
     }
 }
