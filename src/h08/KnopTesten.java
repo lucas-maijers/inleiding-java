5ea19908ca570224aecdf35d7408125ce70a9fef
lucas-maijers <lucas.maijers@student.rocvf.nl>
2019-09-05 14:01:01 +0200

    H08 Knoppen en Tekstvlakken, Lucas Maijers
    Opdracht 1 Release

---
+++ src/h08/KnopTesten.java
@@ -0,0 +1,61 @@
+package h08;
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class KnopTesten extends Applet {
+    TextField tekstvak;
+    Button knop;
+    String output="Je tekst is gereset.";
+    Button reset;
+
+    public void init() {
+        // Invulknop
+        tekstvak = new TextField("", 30);
+        knop = new Button("Ok");
+        // Resetknop
+        reset = new Button("Reset");
+        // Creeërt de Classes
+        knop.addActionListener( new KnopListener() );
+        reset.addActionListener( new ResetknopListener() );
+        // Voegt het toe
+        add(tekstvak);
+        add(knop);
+        add(reset);
+        // Output van Tekstvlak
+        output = "";
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Type een hele lange tekst " +
+                "in het tekstvakje " +
+                "en klik op Ok", 50, 60 );
+        g.drawString("Dit heb jij in het tekstvak gezet: "+output,50,95);
+        g.drawString("Hierboven staat de tekst die jij in het tekstvak hebt gezet.",50,125);
+    }
+
+    class KnopListener implements ActionListener	{
+        public void actionPerformed( ActionEvent e ) {
+            output = tekstvak.getText();
+            repaint();
+        }
+    }
+
+    class ResetknopListener implements ActionListener    {
+        public void actionPerformed( ActionEvent e ) {
+            output = "Je tekstvak wordt gereset";
+            tekstvak.setText("Je tekst wordt over 5 Seconden gereset.");
+            repaint();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            tekstvak.setText("");
+            output = "";
+            repaint();
+        }
+    }
+
+}
