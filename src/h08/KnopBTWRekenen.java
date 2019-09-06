a56dc70e2f107bcdc00d8789911ace69281b3d3e
lucas-maijers <lucas.maijers@student.rocvf.nl>
2019-09-06 11:25:08 +0200

    H08 BTWRekenen

---
+++ src/h08/KnopBTWRekenen.java
@@ -0,0 +1,44 @@
+package h08;
+
+import javax.swing.*;
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class KnopBTWRekenen extends Applet {
+    Label label;
+    TextField tekstvak;
+    Button bereken;
+    double aantal;
+
+    public void init() {
+        tekstvak = new TextField("",20);
+        label = new Label("Typ een bedrag");
+        bereken = new Button("Ok");
+        tekstvak.addActionListener(new TekstvakListener());
+        bereken.addActionListener(new berekenListener());
+        add(label);
+        add(tekstvak);
+        add(bereken);
+    }
+    public void paint(Graphics g) {
+        g.drawString("Bedrag (INCL 21% BTW) €" + aantal * 1.21,50,40);
+    }
+
+    // Enterknop Listener
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String s = tekstvak.getText();
+            aantal = Double.parseDouble( s );
+            repaint();
+        }
+    }
+    // Knop Listener
+    class berekenListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String s = tekstvak.getText();
+            aantal = Double.parseDouble(s);
+            repaint();
+        }
+    }
+}
