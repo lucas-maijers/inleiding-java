5abf84c1d0cad36a9c9bdb6e59432a0806d5425c
lucas-maijers <lucas.maijers@student.rocvf.nl>
2019-09-09 10:21:33 +0200

    H10 Hoogste Getal

---
+++ src/h10/BeslissingenHoogsteGetal.java
@@ -0,0 +1,38 @@
+package h10;
+
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class BeslissingenHoogsteGetal extends Applet {
+    TextField tekstvak;
+    Label label;
+    String tekst, s;
+    int hoogstegetal, getal;
+
+    public void init() {
+        tekstvak = new TextField("",20);
+        tekstvak.addActionListener(new TekstvakListener());
+        label = new Label("Voer hier een getal in");
+        tekst = "";
+        hoogstegetal = 0;
+        add(label);
+        add(tekstvak);
+    }
+    public void paint(Graphics g) {
+        g.drawString("Het hoogstegetal wat u heeft ingevuld is: "+hoogstegetal,50,65);
+    }
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            s = tekstvak.getText();
+            getal = Integer.parseInt(s);
+            if (getal >= hoogstegetal) {
+                hoogstegetal = getal;
+                repaint();
+            }
+            else {
+                return;
+            }
+        }
+    }
+}
