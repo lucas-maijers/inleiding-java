e6e6d38c46b8b633cb2a7d3a55de167d3de90cca
lucas-maijers <lucas.maijers@student.rocvf.nl>
2019-09-10 13:06:48 +0200

    H11 Opdracht 5

---
+++ src/h11/HerhalingVierkantFiguur.java
@@ -0,0 +1,23 @@
+// Opdracht 5
+package h11;
+
+import java.applet.*;
+import java.awt.*;
+
+public class HerhalingVierkantFiguur extends Applet {
+    public void init() {}
+
+    public void paint(Graphics g) {
+        int x = 0;
+        int y = 0;
+        int teller = 0;
+
+        while (teller < 5) {
+            x +=20;
+            y +=20;
+            g.drawRect(x,y,20,20);
+            teller++;
+        }
+
+    }
+}
