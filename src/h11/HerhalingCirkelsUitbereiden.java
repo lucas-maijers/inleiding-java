c8ca5b99fad29064c4a6187e9a891226576930aa
lucas-maijers <lucas.maijers@student.rocvf.nl>
2019-09-10 13:26:32 +0200

    H11 Opdracht 6 Simplificeren en Opdracht 7

--- src/h11/HerhalingCirkelsUitbereiden.java
+++ src/h11/HerhalingCirkelsUitbereiden.java
@@ -1,22 +1,22 @@
-// Opdracht 06
+// Opdracht 07
 package h11;
 
 import java.applet.*;
 import java.awt.*;
 
-public class HerhalingCirkels extends Applet {
+public class HerhalingCirkelsUitbereiden extends Applet {
     public void init() {}
 
     public void paint(Graphics g) {
         int x = 0;
         int y = 0;
         int teller = 0;
-        int aantal = 5;
+        int aantal = 50;
 
         while (teller < aantal) {
             x +=5;
             y +=5;
-            g.drawOval(100-x,100-y, (x * 2), (y * 2));
+            g.drawOval(300-x,300-y,(x*2),(y*2));
             teller ++;
         }
     }
