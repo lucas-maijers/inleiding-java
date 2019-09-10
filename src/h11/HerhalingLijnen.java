c037162a7d5bbb08ca80846a24feb5c643c8bb61
lucas-maijers <lucas.maijers@student.rocvf.nl>
2019-09-10 11:42:07 +0200

    H11 Opdracht 1

---
+++ src/h11/HerhalingLijnen.java
@@ -0,0 +1,21 @@
+// Opdracht 1
+package h11;
+
+import java.applet.*;
+import java.awt.*;
+
+public class HerhalingLijnen extends Applet {
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+            int teller = 0;
+            int x = 0;
+
+            while(teller < 10) {
+                x += 20;
+                g.drawLine(x , 20, x, 200 );
+                teller++;
+        }
+    }
+}
