dc72d90a9d07330654eeab57f599d2aa5a9e6a1b
lucas-maijers <lucas.maijers@student.rocvf.nl>
2019-09-16 10:35:31 +0200

    H12 Opdracht 05

---
+++ src/h12/TabellenStopZoeken.java
@@ -0,0 +1,28 @@
+// Opdracht 05
+package h12;
+
+import java.applet.*;
+import java.awt.*;
+
+public class TabellenStopZoeken extends Applet {
+    double[] salaris = { 100.0, 200.0, 500.0, 400.0, 300.0 };
+    double maximum;
+
+    public void init() {
+        boolean gevonden = false;
+        maximum = salaris[0];
+        for (int teller=0; teller < salaris.length; teller ++) {
+            if(salaris[teller] > maximum) {
+                maximum=salaris[teller-1];
+                gevonden = true;
+                teller++;
+            } else if (gevonden && maximum < salaris[teller]) {
+                gevonden = false;
+            }
+        }
+    }
+
+    public void paint(Graphics g)  {
+        g.drawString("Het maximum is: " + maximum, 50, 20);
+    }
+}
