ba8ac84d6ab213122b8fbad202f90b23a7b51a25
lucas-maijers <lucas.maijers@student.rocvf.nl>
2019-09-18 10:12:40 +0200

    H13 Opdracht 01

--- src/h13/MethodesDriehoek.java
+++ src/h13/MethodesDriehoek.java
@@ -10,12 +10,11 @@ public class MethodesDriehoek extends Applet {
     }
 
     public void paint(Graphics g) {
-        tekenDriehoek(g, 50, 100, 75, 100, 100, 50);
+        tekenDriehoek(g, 50, 100, 100, 100, 75, 50);
 
     }
 
-    void tekenDriehoek(Graphics g, int x1, int x2, int x3, int y1, int y2, int y3) {
-
+    void tekenDriehoek(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
         g.drawLine(x1, y1, x2, y2);
         g.drawLine(x2, y2, x3, y3);
         g.drawLine(x3, y3, x1, y1);
